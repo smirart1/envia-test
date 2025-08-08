c4612d0dc4fdbfc4e3bea16ec015e6b3c498b6e3
Saira M <sairamiranda1@gmail.com>
2025-08-08 17:21:51 -0600

    feat: add products view and product card component

---
+++ src/components/products/ProductCard.vue
@@ -0,0 +1,34 @@
+<template>
+  <v-card
+    variant="flat"
+    class="mx-auto"
+    max-width="400"
+    @click="$router.push({ name: 'details', params: { id: product.id } })"
+  >
+    <v-img cover rounded aspect-ratio="1" :src="product.image" lazy-src="/images/placeholder.png">
+      <template v-slot:placeholder>
+        <div class="d-flex align-center justify-center fill-height">
+          <v-progress-circular color="grey-lighten-4" indeterminate />
+        </div>
+      </template>
+    </v-img>
+    <v-card-text class="pa-0">
+      <v-row no-gutters>
+        <v-col cols="12" class="text-caption app-font mt-1 mb-n1 text-grey-darken-1 text-truncate">
+          {{ product.vendor }}
+        </v-col>
+        <v-col cols="12" class="text-subtitle-2 app-font text-truncate"> {{ product.name }} </v-col>
+        <v-col cols="12" class="font-weight-bold app-font text-truncate">
+          {{ product.price }}
+        </v-col>
+      </v-row>
+    </v-card-text>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+import type { Product } from '@/types'
+defineProps<{
+  product: Product
+}>()
+</script>
---
+++ src/components/products/ProductsPanel.vue
@@ -0,0 +1,29 @@
+<template>
+  <v-row no-gutters class="pa-2 pt-4">
+    <v-col
+      v-for="product in productsStore.products"
+      cols="12"
+      sm="6"
+      md="4"
+      lg="3"
+      class="pa-2"
+      :key="product.id"
+    >
+      <ProductCard :product="product" />
+    </v-col>
+  </v-row>
+</template>
+
+<script setup lang="ts">
+import { useProductsStore } from '@/stores'
+import { onMounted } from 'vue'
+import { storeToRefs } from 'pinia'
+import ProductCard from './ProductCard.vue'
+
+const productsStore = useProductsStore()
+const { products } = storeToRefs(productsStore)
+
+onMounted(async () => {
+  products.value.length === 0 && (await productsStore.getProducts())
+})
+</script>
