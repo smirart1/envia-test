f5c269250aa7ca2fdd8c1d8710b2e61f83ae1ef6
Saira M <sairamiranda1@gmail.com>
2025-08-08 17:03:09 -0600

    shore: add vuetify and theme configuration

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import vuetify from '@/plugins/vuetify'
+
+import '@/assets/styles.css'
 
 const app = createApp(App)
+const pinia = createPinia()
 
-app.use(createPinia())
+app.use(pinia)
 app.use(router)
+app.use(vuetify)
 
 app.mount('#app')
---
+++ src/plugins/vuetify.ts
@@ -0,0 +1,21 @@
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
+export default createVuetify({
+  components,
+  directives,
+  theme: {
+    themes: {
+      light: {
+        colors: {
+          primary: '#9ACD0A',
+          secondary: '#373737',
+          error: '#FF5252',
+        },
+      },
+    },
+  },
+})
