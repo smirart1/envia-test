4a1730d7750594ac243a931823cf5a71cf6e245d
Saira M <sairamiranda1@gmail.com>
2025-08-08 17:07:04 -0600

    chore: configure app routes for products, product details and cart

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,44 @@ import { createRouter, createWebHistory } from 'vue-router'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [],
+  routes: [
+    {
+      path: '',
+      redirect: { name: 'products' },
+    },
+    {
+      path: '/products',
+      component: () => import('@/views/ProductsView.vue'),
+      children: [
+        {
+          path: '',
+          name: 'products',
+          component: () => import('@/components/products/ProductsPanel.vue'),
+        },
+        {
+          path: ':id',
+          name: 'details',
+          component: () => import('@/components/products/DetailsPanel.vue'),
+          props: true,
+        },
+      ],
+    },
+    {
+      path: '/cart',
+      component: () => import('@/views/CartView.vue'),
+      children: [
+        {
+          path: '',
+          name: 'cart',
+          component: () => import('@/components/cart/CartPanel.vue'),
+        },
+      ],
+    },
+    {
+      path: '/:path(.*)',
+      redirect: { name: 'products' },
+    },
+  ],
 })
 
 export default router
